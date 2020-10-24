dc0177d19117175e8688573671606a47a05b12d5
miroslawmajka <go2mirek@gmail.com>
2020-10-24 19:41:24 +0100

    Added jest, coverage, pipelines and sample math modules with tests

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['cobertura', 'text', 'lcov', 'clover'],
+  reporters: ['default', 'jest-junit'],
+  testEnvironment: 'node',
+};
